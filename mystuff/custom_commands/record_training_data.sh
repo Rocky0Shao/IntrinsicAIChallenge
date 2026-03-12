da2654901bafd60150d1bc98c27492b35e09fa52
Rocky Shao <rockyshao22@gmail.com>
2026-03-12 11:11:54 -0400

    lerobot record & hugging face push working. more custom commands

--- mystuff/custom_commands/record_training_data.sh
+++ mystuff/custom_commands/record_training_data.sh
@@ -5,7 +5,7 @@ pixi run lerobot-record \
   --robot.teleop_target_mode=cartesian --robot.teleop_frame_id=gripper/tcp \
   --dataset.repo_id=rockyshao22/Intrinsic_AI \
   --dataset.single_task="insert plug" \
-  --dataset.push_to_hub=false \
+  --dataset.push_to_hub=true \
   --dataset.private=true \
   --play_sounds=false \
   --display_data=true
--- mystuff/custom_commands/teleop.sh
+++ mystuff/custom_commands/teleop.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
 # Prompt the user for the trial number
-read -p "Which trial do you want to record? (1, 2, or 3): " trial_num
+read -p "Which trial do you want to teleop? (1, 2, or 3): " trial_num
 
-# Base recording command using your repo ID and standard flags
-BASE_CMD="pixi run lerobot-record \
+# Base teleop command using your repo ID and standard flags
+BASE_CMD="pixi run lerobot-teleoperate \
   --robot.type=aic_controller --robot.id=aic \
   --teleop.type=aic_cheatcode --teleop.id=aic \
   --robot.teleop_target_mode=cartesian --robot.teleop_frame_id=gripper/tcp \
-  --dataset.repo_id=rockyshao22/Intrinsic_AI \
-  --dataset.single_task=\"insert plug\" \
-  --dataset.push_to_hub=true \
-  --dataset.private=true \
-  --play_sounds=false \
-  --display_data=true \
-  --resume=true"
+  --display_data=true"
 
 # Execute the command with the correct trial-specific TF frames
 case $trial_num in
   1)
-    echo "Starting LeRobot recording for Trial 1..."
+    echo "Starting LeRobot teleop for Trial 1..."
     eval "$BASE_CMD \
       --teleop.task_cable_name=cable_0 \
       --teleop.task_plug_name=sfp_tip \
@@ -27,7 +21,7 @@ case $trial_num in
       --teleop.task_port_name=sfp_port_0"
     ;;
   2)
-    echo "Starting LeRobot recording for Trial 2..."
+    echo "Starting LeRobot teleop for Trial 2..."
     eval "$BASE_CMD \
       --teleop.task_cable_name=cable_0 \
       --teleop.task_plug_name=sfp_tip \
@@ -35,7 +29,7 @@ case $trial_num in
       --teleop.task_port_name=sfp_port_0"
     ;;
   3)
-    echo "Starting LeRobot recording for Trial 3..."
+    echo "Starting LeRobot teleop for Trial 3..."
     eval "$BASE_CMD \
       --teleop.task_cable_name=cable_1 \
       --teleop.task_plug_name=sc_tip \
